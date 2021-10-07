8805004b3e6fb724b3f4e5ac65e4a9b9c289f982
nome <gustavoiafelix@gmail.com>
2021-10-07 18:50:10 -0300

    HTTP Codes - Modified[2]

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@
 
 const express = require('express');
 const app = express();
+const { v4: uuidv4 } = require('uuid');
 const PORT = 3333;
 
 const users = [];
-const { v4: uuidv4 } = require('uuid');
+
 app.use(express.json());
 
 function verifyUsername(request, response, next) {
@@ -78,7 +79,7 @@ app.put('/todos/:id', verifyUsername, (request, response) => {
     const todo = usernameExists.todos.find(todo => todo.id == id)
 
     if (!todo) {
-        return response.status(404).json({ error: 'Not found' })
+        return response.status(404).json({ error: "Not found" })
     }
 
     todo.title = title;
@@ -89,34 +90,36 @@ app.put('/todos/:id', verifyUsername, (request, response) => {
 })
 
 app.patch('/todos/:id/done', verifyUsername, (request, response) => {
-            const { usernameExists } = request;
-            const { id } = request.params;
+    const { usernameExists } = request;
+    const { id } = request.params;
 
-            const todo = usernameExists.todos.find(todo => todo.id == id)
+    const todo = usernameExists.todos.find(todo => todo.id == id)
 
-            if (!todo) {
-                return response.status(404).json({
-                        error: 'Not found })
-                    }
+    if (!todo) {
+        return response.status(404).json({
+            error: "Not found"
+        })
+    }
 
-                    todo.done = true;
+    todo.done = true;
 
-                    return response.json(todo);
-                })
+    return response.json(todo);
+})
 
-            app.delete('/todos/:id', verifyUsername, (request, response) => {
-                const { usernameExists } = request;
-                const { id } = request.params;
+app.delete('/todos/:id', verifyUsername, (request, response) => {
+    const { usernameExists } = request;
+    const { id } = request.params;
 
-                const index = usernameExists.todos.findIndex(todo => todo.id == id)
+    const index = usernameExists.todos.findIndex(todo => todo.id == id)
 
-                if (index == -1) {
-                    return response.status(404).json({ error: 'Not found' })
-                }
+    if (index == -1) {
+        return response.status(404).json({ error: "Not found" })
+    }
 
-                usernameExists.todos.splice(index, 1);
+    usernameExists.todos.splice(index, 1);
 
-                return response.status(204).send()
-            })
+    return response.status(204).send()
+})
 
-            app.listen(PORT);
+app.listen(PORT);
+console.info(`Server created on PORT : ${PORT}`)
